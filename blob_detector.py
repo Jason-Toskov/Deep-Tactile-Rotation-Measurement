8b3e0c978f5bea82ea1e54653a3f49aa46aaa104
Rhys <rhys.newbury@monashmotorsport.com>
2022-01-19 11:14:18 +1100

    refactor

--- blob_detector.py
+++ blob_detector.py
@@ -12,6 +12,7 @@ from grasp_executor.srv import AngleTrack
 
 from enum import Enum
 
+
 class Quadrant(Enum):
     INIT = 0
     NW = 1
@@ -20,20 +21,35 @@ class Quadrant(Enum):
     NE = 4
 
 
-class AngleDetector():
+class AngleDetectorService:
     def __init__(self):
         rospy.init_node("Angle_detector")
-        #self.image_topic = "/realsense/rgb"
+        # self.image_topic = "/realsense/rgb"
+
+        self.AD = AngleDetector()
+        # self.current_image = None
+        # rospy.Subscriber(self.image_topic, Image, self.image_callback)
+        rospy.Service("track_angle", AngleTrack, self.AD.update_angle)
+        rospy.Service("reset_angle_tracking", Empty, self.AD.reset_tracking)
+
+        rospy.loginfo("Angle tracker ready!")
+        rospy.spin()
+
+
+class AngleDetector:
+    def __init__(self):
+        rospy.init_node("Angle_detector")
+        # self.image_topic = "/realsense/rgb"
 
         self.bridge = CvBridge()
         self.state = Quadrant.INIT
         self.closest_state = Quadrant.INIT
         self.angle = None
 
-        #self.current_image = None
-        #rospy.Subscriber(self.image_topic, Image, self.image_callback)
-        rospy.Service('track_angle', AngleTrack, self.update_angle)
-        rospy.Service('reset_angle_tracking', Empty, self.reset_tracking)
+        # self.current_image = None
+        # rospy.Subscriber(self.image_topic, Image, self.image_callback)
+        rospy.Service("track_angle", AngleTrack, self.update_angle)
+        rospy.Service("reset_angle_tracking", Empty, self.reset_tracking)
 
         rospy.loginfo("Angle tracker ready!")
         rospy.spin()
@@ -45,14 +61,20 @@ class AngleDetector():
         self.state = Quadrant.INIT
         self.closest_state = Quadrant.INIT
         self.angle = None
-        print('reset!')
+        print("reset!")
         return EmptyResponse()
 
     def angle_calculation(self, point0, point1):
-        temp_angle = np.arctan(np.abs(float(point0[1]-point1[1])/float(point0[0]-point1[0])))*180/np.pi
-        print((point0[1]-point1[1]))
-        print((point0[0]-point1[0]))
-        print(np.abs((point0[1]-point1[1])/(point0[0]-point1[0])))
+        temp_angle = (
+            np.arctan(
+                np.abs(float(point0[1] - point1[1]) / float(point0[0] - point1[0]))
+            )
+            * 180
+            / np.pi
+        )
+        print((point0[1] - point1[1]))
+        print((point0[0] - point1[0]))
+        print(np.abs((point0[1] - point1[1]) / (point0[0] - point1[0])))
         print(self.state)
 
         if self.state == Quadrant.NW:
@@ -70,7 +92,9 @@ class AngleDetector():
 
         if self.state == Quadrant.INIT:
             self.state = Quadrant.NW if rightmost_point == lowest_point else Quadrant.SW
-            self.closest_state = Quadrant.SW if rightmost_point == lowest_point else Quadrant.NW
+            self.closest_state = (
+                Quadrant.SW if rightmost_point == lowest_point else Quadrant.NW
+            )
         elif self.state == Quadrant.NW:
             print(rightmost_point, lowest_point, self.state, self.closest_state)
             if rightmost_point is not lowest_point:
@@ -85,18 +109,16 @@ class AngleDetector():
             if rightmost_point is lowest_point:
                 self.state = self.closest_state
 
-
     def closest_new_state(self):
         self.closest_state = Quadrant.INIT
         if self.state == Quadrant.NW:
-            self.closest_state = Quadrant.SW if self.angle > -45 else Quadrant.NE  
+            self.closest_state = Quadrant.SW if self.angle > -45 else Quadrant.NE
         elif self.state == Quadrant.SW:
             self.closest_state = Quadrant.NW if self.angle < 45 else Quadrant.SE
         elif self.state == Quadrant.SE:
             self.closest_state = Quadrant.SW if self.angle < 135 else Quadrant.NE
         elif self.state == Quadrant.NE:
             self.closest_state = Quadrant.NW if self.angle > -135 else Quadrant.SE
-
 
     def update_angle(self, req):
         # rate = rospy.Rate(0.5)
@@ -108,32 +130,31 @@ class AngleDetector():
 
         print("Reached loop!")
         # image = cv2.imread('img_temp.jpeg')
-        im = self.bridge.imgmsg_to_cv2(req.im, desired_encoding='8UC3')
+        im = self.bridge.imgmsg_to_cv2(req.im, desired_encoding="8UC3")
         result = im.copy()
         image = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-        
-        lower = np.array([0,85,0])
-        upper = np.array([7,255,255])
+
+        lower = np.array([0, 85, 0])
+        upper = np.array([7, 255, 255])
         mask = cv2.inRange(image, lower, upper)
 
-
-        lower1 = np.array([175,85,0])
-        upper1 = np.array([180,255,255])
+        lower1 = np.array([175, 85, 0])
+        upper1 = np.array([180, 255, 255])
         mask1 = cv2.inRange(image, lower1, upper1)
 
         mask = mask + mask1
 
         result = cv2.bitwise_and(result, result, mask=mask)
 
-        cv2.imshow('orig', im)
-        cv2.imshow('hsv', image)
-        cv2.imshow('mask', mask)
+        cv2.imshow("orig", im)
+        cv2.imshow("hsv", image)
+        cv2.imshow("mask", mask)
 
-        cv2.imwrite('img_temp.jpeg', im)
+        cv2.imwrite("img_temp.jpeg", im)
 
-        cv2.imwrite('mask.jpeg', mask)
+        cv2.imwrite("mask.jpeg", mask)
         # cv2.imshow('result', result)
-        cv2.imwrite('result.jpeg', result)
+        cv2.imwrite("result.jpeg", result)
 
         # params = cv2.SimpleBlobDetector_Params()
         # params.filterByArea = True
@@ -146,7 +167,9 @@ class AngleDetector():
         # cv2.imshow("Keypoints", im_with_keypoints)
         # cv2.waitKey(0)
 
-        _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        _, contours, _ = cv2.findContours(
+            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+        )
         # blob = max(contours, key=lambda el: cv2.contourArea(el))
         # print(type(contours))
         contours.sort(key=lambda el: cv2.contourArea(el), reverse=True)
@@ -157,13 +180,13 @@ class AngleDetector():
         center1 = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
         # print(center)
         canvas = result.copy()
-        cv2.circle(canvas, center1, 2, (0,255,0), -1)
+        cv2.circle(canvas, center1, 2, (0, 255, 0), -1)
 
         blob2 = blob_list[1]
         M = cv2.moments(blob2)
         center2 = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
         # print(center)
-        cv2.circle(canvas, center2, 2, (0,255,0), -1)
+        cv2.circle(canvas, center2, 2, (0, 255, 0), -1)
 
         # print(center1)
         # print(center2)
@@ -187,21 +210,19 @@ class AngleDetector():
 
         print(self.angle)
 
-
-        cv2.imshow('canvas', canvas)
-        cv2.imwrite('canvas.jpeg', canvas)
+        cv2.imshow("canvas", canvas)
+        cv2.imwrite("canvas.jpeg", canvas)
         cv2.waitKey()
 
+        # cv2.waitKey()
 
-            # cv2.waitKey()
-
-            # rate.sleep()
+        # rate.sleep()
 
         return self.angle
 
 
 if __name__ == "__main__":
     try:
-        angle_class = AngleDetector()
+        angle_class = AngleDetectorService()
     except KeyboardInterrupt:
         pass
