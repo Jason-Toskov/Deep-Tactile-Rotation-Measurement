c411439e73809912a722f9577cb75ef8276205ba
jtos0003 <jtos0003@student.monash.edu>
2022-01-20 15:04:16 +1100

    tuned

--- Controller.py
+++ Controller.py
@@ -21,9 +21,10 @@ from blob_detector import AngleDetector
 
 class AdaptiveController:
     def __init__(self):
+        rospy.init_node("Controller")
 
-        self.AD = AngleDetector(writeImages=False, showImages=False)
-        self.goal = 30
+        self.AD = AngleDetector(writeImages=False, showImages=False, cv2Image=True)
+        self.goal = 60
         self.initialiseCamera()
 
         self.gripper_sub = rospy.Subscriber(
@@ -37,48 +38,67 @@ class AdaptiveController:
             queue_size=1,
         )
 
+
+        # bring camera in to focus
         for _ in range(20):
-            self.AD.update_angle(self.getImage)
+            img = self.qRgb.get().getCvFrame()
+            
+        # initialise things
+        for _ in range(10):
+            self.AD.update_angle(self.qRgb.get().getCvFrame())
 
         self.angle = self.AD.getAngle
         self.angularVelocity = self.AD.getAngularVelocity
         self.gripper_data = None
         self.last_move_time = 0
 
+        self.PUBLISH = True
+
         while not (self.gripper_data or rospy.is_shutdown()):
             rospy.sleep(1)
             rospy.loginfo("Waiting for gripper!")
 
         arrived = False
+        t1 = timeit.default_timer()
         while not arrived and not rospy.is_shutdown():
             arrived = self.control_loop()
+            t2 = timeit.default_timer()  
+            print(round(1 / (t2 - t1),2), round(self.angularVelocity(),2), round(self.angle(), 2))
+            t1 = timeit.default_timer()
+
+        while not rospy.is_shutdown():
+            image = self.qRgb.get().getCvFrame()
+            self.AD.update_angle(image)
+            print(round(self.angularVelocity(),2), round(self.angle(), 2))
+
+        print("reached goal!!")
 
     def initialiseCamera(self):
 
-        pipeline = dai.Pipeline()
+        self.pipeline = dai.Pipeline()
 
         # Define source and output
-        camRgb = pipeline.create(dai.node.ColorCamera)
-        xoutRgb = pipeline.create(dai.node.XLinkOut)
+        self.camRgb = self.pipeline.create(dai.node.ColorCamera)
+        self.xoutRgb = self.pipeline.create(dai.node.XLinkOut)
 
-        xoutRgb.setStreamName("rgb")
+        self.xoutRgb.setStreamName("rgb")
 
         # Properties
-        camRgb.setPreviewSize(300, 300)
-        camRgb.setInterleaved(False)
-        camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-        camRgb.setFps(60)
+        self.camRgb.setPreviewSize(300, 300)
+        self.camRgb.setInterleaved(False)
+        self.camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+        self.camRgb.setFps(60)
 
-        camRgb.preview.link(xoutRgb.input)
+        self.camRgb.preview.link(self.xoutRgb.input)
 
-        device = dai.Device(pipeline)
-        qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-        self.getImage = qRgb.get().getCvFrame
+        self.device = dai.Device(self.pipeline)
+        self.qRgb = self.device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+
 
     def control_loop(self):
-        time_delay = 0
+        time_delay = 0.05
 
-        image = self.getImage()
+        image = self.qRgb.get().getCvFrame()
         self.AD.update_angle(image)
 
         # future predict the angle of the object, offset by time delay
@@ -86,40 +106,48 @@ class AdaptiveController:
 
         current_time = timeit.default_timer()
 
-        if (self.goal - object_angle) < 0.01:
+        if abs(self.goal - object_angle) < 1 or self.angle() > self.goal:
+            print("sent stopped!!")
             self.close_gripper()
             return True
 
         canMove = (current_time - self.last_move_time) > 0.1
 
         # if the object is slow, and the gripper has opened
-        if self.angularVelocity < 0.3 and canMove:
+        if self.angularVelocity() < 0.3 and canMove:
             self.slightly_open_gripper()
 
         return False
 
     def slightly_open_gripper(self):
         command = outputMsg.Robotiq2FGripper_robot_output()
-        command.rPR = self.gripperWidth - 1
+        command.rPR = self.gripper_width - 1
         command.rACT = 1
         command.rGTO = 1
         command.rSP = 255
         command.rFR = 150
+        print("open a lil!!")
 
         self.last_move = timeit.default_timer()
-
-        self.gripper_pub.pub(command)
+        
+        if self.PUBLISH:
+            self.gripper_pub.publish(command)
 
     def gripper_state_callback(self, data):
         self.gripper_data = data
         self.gripper_width = data.gPR
 
-    def close_gripper(self):
+    def close_gripper(self, width=255):
         command = outputMsg.Robotiq2FGripper_robot_output()
-        command.rPR = 255
+        command.rPR = width
         command.rACT = 1
         command.rGTO = 1
         command.rSP = 255
         command.rFR = 150
 
-        self.gripper_pub.pub(command)
+        if self.PUBLISH:
+            self.gripper_pub.publish(command)
+
+if __name__ == "__main__":
+    a = AdaptiveController()
+    rospy.spin()
