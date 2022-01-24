0efe416ff4357658594414ae77e1f9d15cafb100
jtos0003 <jtos0003@student.monash.edu>
2022-01-24 16:03:58 +1100

    Integrated wandb

--- lstm_model/lstm.py
+++ lstm_model/lstm.py
@@ -1,11 +1,16 @@
 import torch
+from torch import random
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import copy
+import time
+import wandb
+
+wandb.init(project="SRP")
 
 class TactileDataset(Dataset):
     def __init__(self, path, mode="train"):   #, max_seq_length=30):
@@ -19,8 +24,8 @@ class TactileDataset(Dataset):
     
     def __getitem__(self, i):
         df = pd.read_csv('./' + self.datapoints[i])
-        row, col = df.shape
-        final_angle = df.iloc[-1:]['true_angle']
+        # row, col = df.shape
+        # final_angle = df.iloc[-1:]['true_angle']
 
         df_tensor = torch.Tensor(df.values).float()
         
@@ -31,7 +36,7 @@ class TactileDataset(Dataset):
         #     padding[:, -1] = final_angle
         #     df = df.append
 
-        return df_tensor[:,0:-1], df_tensor[:,-1] - df_tensor[0,-1]
+        return df_tensor[:,0:-2], (df_tensor[:,-2] - df_tensor[0,-2])/90
         
         
 
@@ -47,7 +52,8 @@ class RegressionLSTM(nn.Module):
             input_size = self.num_features,
             hidden_size = self.hidden_size,
             batch_first = True,
-            num_layers = self.num_layers
+            num_layers = self.num_layers,
+            dropout = 0.2
         )
         
         self.linear = nn.Linear(in_features=self.hidden_size, out_features=1)
@@ -85,23 +91,60 @@ def train(device, loader, model, loss_func, optim, l1loss):
     
     return loss_count, abs_error_count
 
+def test(device, loader, model, loss_func, optim, l1loss):
+    model.eval()
+
+    loss_count = 0
+    abs_error_count = 0
+
+    with torch.no_grad():
+        for i, (features, label) in enumerate(loader):
+            out = model(features.to(device))
+            loss = loss_func(torch.squeeze(out), torch.squeeze(label.to(device)))
+            l1error = l1loss(torch.squeeze(out), torch.squeeze(label.to(device)))
+            
+            loss_count += loss.item()
+            abs_error_count += l1error.item()
+            
+            # optim.zero_grad()
+            # loss.backward()
+            # optim.step()
+            
+        loss_count /= i+1
+        abs_error_count /= i+1
+    
+    return loss_count, abs_error_count
+
+
 def main():
     lr = 1e-4
     num_epochs = 1200
+    train_frac = 0.8
+
+    wandb.config = {
+        "learning_rate": lr,
+        "epochs": num_epochs,
+        "train_fraction": train_frac
+    }
     
     #Set device to GPU_indx if GPU is avaliable
     GPU_indx = 0
     device = torch.device(GPU_indx if torch.cuda.is_available() else 'cpu')
     
-    path = './Dataset_usbc_box/'
+    path = './Data/'
     
     data = TactileDataset(path, mode='train')
-    loader = DataLoader(data, batch_size = 1, shuffle=True)
+    train_data_length = round(len(data)*train_frac)
+    test_data_length = len(data) - train_data_length
+    train_data, test_data = random_split(data, [train_data_length, test_data_length], generator=torch.Generator().manual_seed(42))
+
+    train_loader = DataLoader(train_data, batch_size = 1, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size = 1, shuffle=True)
     
-    features, label = next(iter(loader))
+    # features, label = next(iter(train_data))
     
     
-    model = RegressionLSTM(device, 142, 200, 1)
+    model = RegressionLSTM(device, 142, 200, 2)
     model = model.to(device)
     # out = model(features.to(device))
     
@@ -109,62 +152,105 @@ def main():
     optim = torch.optim.Adam(model.parameters(), lr=lr)
     l1loss = nn.L1Loss()
     
-    loss_array = []
-    error_array = []
+    loss_array_train = []
+    error_array_train = []
+    loss_array_test= []
+    error_array_test = []
     best_model = copy.deepcopy(model)
     lowest_error = 1e5
+    old_time = time.time()
     for i in range(num_epochs):
-        loss_current, abs_error_current = train(device, loader, model, loss_func, optim, l1loss)
-        loss_array.append(loss_current)
-        error_array.append(abs_error_current)
-        print("Epoch: %i, Absolute error: %f, Loss: %f" % (i, abs_error_current, loss_current) )
-        
-        if abs_error_current < lowest_error:
+        loss_train, abs_error_train = train(device, train_loader, model, loss_func, optim, l1loss)
+        loss_array_train.append(loss_train)
+        error_array_train.append(abs_error_train*90)
+
+        loss_test, abs_error_test = test(device, test_loader, model, loss_func, optim, l1loss)
+        loss_array_test.append(loss_test)
+        error_array_test.append(abs_error_test*90)
+
+        wandb.log({
+            "loss/train":loss_train,
+            "loss/test":loss_test,
+            "abs_error/train":abs_error_train*90,
+            "abs_error/test":abs_error_test*90,
+        })
+
+        new_time = time.time()
+        print("Epoch: %i, Absolute error: %f, Loss: %f, Time taken: %f" % (i, abs_error_train*90, loss_train, new_time-old_time) )
+        old_time = copy.deepcopy(new_time)
+        if abs_error_train < lowest_error:
             # print("new best! old best: %f, new best: %f"%(lowest_error, abs_error_current))
             best_model_dict = copy.deepcopy(model.state_dict())
-            lowest_error = abs_error_current
+            torch.save(model.state_dict(), '../../best_model.pt')
+            lowest_error = abs_error_train
             print("new best!")
     
     print("Lowest error was: %f"%(lowest_error))
-    best_model = RegressionLSTM(device, 142, 200, 1)
+    best_model = RegressionLSTM(device, 142, 200, 2)
     best_model.load_state_dict(best_model_dict)
     best_model = best_model.to(device)
+
         
     # breakpoint()
     #plot that shows labels/out of some sequences
     fig, axs = plt.subplots(2, 2)
-    loader = DataLoader(data, batch_size = 1, shuffle=True)
+    train_loader = DataLoader(train_data, batch_size = 1, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size = 1, shuffle=True)
     # breakpoint()
     
     for ax in axs.flat:
-        features, label = next(iter(loader))
+        features, label = next(iter(train_loader))
         while(max(label.squeeze())-min(label.squeeze()) < 5):
-            features, label = next(iter(loader))
+            features, label = next(iter(train_loader))
         out = best_model(features.to(device))
         out = out.squeeze()
         label = label.squeeze()
         x_range = [*range(len(out))]
-        ax.plot(x_range, label.detach().to('cpu'), label = 'Ground truth')
-        ax.plot(x_range, out.detach().to('cpu'), label = 'Prediction')
+        ax.plot(x_range, label.detach().to('cpu')*90, label = 'Ground truth')
+        ax.plot(x_range, out.detach().to('cpu')*90, label = 'Prediction')
     
-    os.chdir('..')
-    plt.savefig('../examples.png')
+    fig.suptitle("Train examples")
+    # os.chdir('..')
+    plt.savefig('../../examples_train.png')
+    plt.show()
+
+    fig, axs = plt.subplots(2, 2)
+    for ax in axs.flat:
+        features, label = next(iter(test_loader))
+        while(max(label.squeeze())-min(label.squeeze()) < 5):
+            features, label = next(iter(test_loader))
+        out = best_model(features.to(device))
+        out = out.squeeze()
+        label = label.squeeze()
+        x_range = [*range(len(out))]
+        ax.plot(x_range, label.detach().to('cpu')*90, label = 'Ground truth')
+        ax.plot(x_range, out.detach().to('cpu')*90, label = 'Prediction')
+    
+    fig.suptitle("Test examples")
+    # os.chdir('..')
+    plt.savefig('../../examples_test.png')
     plt.show()
     
+
+    os.chdir('..')
 
     plt.title("Loss (MSE)")
     plt.xlabel('Epoch number')
     plt.ylabel('Loss')
-    plt.plot(np.array([*range(num_epochs)])+1, loss_array)
+    plt.plot(np.array([*range(num_epochs)])+1, loss_array_train, label="Train")
+    plt.plot(np.array([*range(num_epochs)])+1, loss_array_test, label="Test")
     plt.grid(True)
+    plt.legend()
     plt.savefig('../loss.png')
     plt.show()
     
     plt.title("Absolute Error")
     plt.xlabel('Epoch number')
     plt.ylabel('Error')
-    plt.plot(np.array([*range(num_epochs)])+1, error_array)
+    plt.plot(np.array([*range(num_epochs)])+1, error_array_train, label="Train")
+    plt.plot(np.array([*range(num_epochs)])+1, error_array_test, label="Test")
     plt.grid(True)
+    plt.legend()
     plt.savefig('../error.png')
     plt.show()
     
