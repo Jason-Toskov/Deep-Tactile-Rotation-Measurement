51856ddf0be78495e202f3e05363dac612e42882
Rhys <rhys.newbury@monashmotorsport.com>
2022-01-27 11:53:58 +1100

    psuedo merge

---
+++ scripts/controller/Controller.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+import numpy as np
+import cv2
+from multiprocessing import Process, Queue
+
+import rospy, sys
+from time import sleep
+from sensor_msgs.msg import Image
+from std_msgs.msg import Empty as EmptyMsg
+from cv_bridge import CvBridge, CvBridgeError
+from std_srvs.srv import Empty, EmptyResponse
+from grasp_executor.srv import AngleTrack
+import depthai as dai
+from robotiq_2f_gripper_control.msg import (
+    _Robotiq2FGripper_robot_output as outputMsg,
+    _Robotiq2FGripper_robot_input as inputMsg,
+)
+import timeit
+
+from rotation_measurement.data_processing.blob_detector import AngleDetector
+
+
+class AdaptiveController:
+    def __init__(self):
+        rospy.init_node("Controller")
+
+        self.AD = AngleDetector(writeImages=False, showImages=False, cv2Image=True)
+        self.goal = 60
+        # self.initialiseCamera()
+
+        self.gripper_sub = rospy.Subscriber(
+            "/Robotiq2FGripperRobotInput",
+            inputMsg.Robotiq2FGripper_robot_input,
+            self.gripper_state_callback,
+        )
+        self.gripper_pub = rospy.Publisher(
+            "/Robotiq2FGripperRobotOutput",
+            outputMsg.Robotiq2FGripper_robot_output,
+            queue_size=1,
+        )
+
+        self.prev_data = None
+
+        self.angle = self.AD.getAngle
+        self.angularVelocity = self.AD.getAngularVelocity
+        self.gripper_data = None
+        self.last_move_time = 0
+
+        self.PUBLISH = True
+
+        while not (self.gripper_data or rospy.is_shutdown()):
+            rospy.sleep(1)
+            rospy.loginfo("Waiting for gripper!")
+
+        self.q = Queue()
+
+        self.p1 = Process(target=self.getCameraFrame)
+        self.p2 = Process(target=self.control)
+
+        self.p1.start()
+        self.p2.start()
+
+        # wait for p2 to finish
+        self.p2.join()
+        # kill the camera
+        # self.p1.kill()
+
+        while not rospy.is_shutdown():
+            image = self.qRgb.get().getCvFrame()
+            self.AD.update_angle(image)
+            # print(round(self.angularVelocity(), 2), round(self.angle(), 2))
+
+    # def initialiseCamera(self):
+
+    def getCameraFrame(self):
+        pipeline = dai.Pipeline()
+
+        # Define source and output
+        camRgb = pipeline.create(dai.node.ColorCamera)
+        xoutRgb = pipeline.create(dai.node.XLinkOut)
+
+        xoutRgb.setStreamName("rgb")
+
+        # Properties
+        camRgb.setPreviewSize(300, 300)
+        camRgb.setInterleaved(False)
+        camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+        camRgb.setFps(60)
+
+        camRgb.preview.link(xoutRgb.input)
+
+        device = dai.Device(pipeline)
+        qRgb = device.getOutputQueue(name="rgb", maxSize=1, blocking=False)
+
+        # bring camera in to focus
+        for _ in range(20):
+            img = qRgb.get().getCvFrame()
+
+        # initialise things
+        for _ in range(10):
+            self.AD.update_angle(qRgb.get().getCvFrame())
+
+        while not rospy.is_shutdown():
+            image = qRgb.get().getCvFrame()
+            self.AD.update_angle(image)
+            self.q.put((timeit.default_timer(), self.angle(), self.angularVelocity()))
+
+    def control(self):
+        arrived = False
+        t1 = timeit.default_timer()
+
+        while not arrived and not rospy.is_shutdown():
+            arrived = self.control_loop()
+
+            t2 = timeit.default_timer()
+            # print(
+            #     round(1 / (t2 - t1), 2),
+            #     round(self.angularVelocity(), 2),
+            #     round(self.angle(), 2),
+            # )
+            t1 = timeit.default_timer()
+
+        print("hello")
+
+    def control_loop(self):
+
+        # camera is not initalised
+        if self.prev_data is None and self.q.empty():
+            return False
+
+        self.prev_data = self.prev_data if self.q.empty() else self.q.get()
+
+        (prev_time, angle, angleVel) = self.prev_data
+
+        time_since_update = max(0, prev_time - self.prev_data[0])
+        time_delay = 0
+
+        # future predict the angle of the object, offset by time delay
+        object_angle = angle + angleVel * (time_delay + time_since_update)
+
+        current_time = timeit.default_timer()
+
+        if abs(self.goal - object_angle) < 1 or angle > self.goal:
+            print(self.goal, angle, object_angle, "sent stopped!!")
+            self.close_gripper()
+            return True
+
+        canMove = (current_time - self.last_move_time) > 0.05
+
+        # if the object is slow, and the gripper has opened
+        if angleVel < 0.3 and canMove:
+            print("loooooosen")
+            self.slightly_open_gripper()
+
+        return False
+
+    def slightly_open_gripper(self):
+        command = outputMsg.Robotiq2FGripper_robot_output()
+        command.rPR = self.gripper_width - 1
+        command.rACT = 1
+        command.rGTO = 1
+        command.rSP = 255
+        command.rFR = 150
+
+        self.last_move = timeit.default_timer()
+
+        if self.PUBLISH:
+            print(command)
+            self.gripper_pub.publish(command)
+
+    def gripper_state_callback(self, data):
+        self.gripper_data = data
+        self.gripper_width = data.gPO
+
+    def close_gripper(self, width=255):
+        command = outputMsg.Robotiq2FGripper_robot_output()
+        command.rPR = width
+        command.rACT = 1
+        command.rGTO = 1
+        command.rSP = 255
+        command.rFR = 150
+
+        if self.PUBLISH:
+            self.gripper_pub.publish(command)
+
+
+if __name__ == "__main__":
+    a = AdaptiveController()
+    rospy.spin()
--- scripts/rotation_measurement/data_processing/blob_detector.py
+++ scripts/rotation_measurement/data_processing/blob_detector.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+from asyncore import write
 import numpy as np
 import cv2
 
-import rospy
+import rospy, timeit
 from time import sleep
 from sensor_msgs.msg import Image
 from std_msgs.msg import Empty as EmptyMsg
@@ -12,6 +13,11 @@ from grasp_executor.srv import AngleTrack
 
 from enum import Enum
 
+import sys
+
+PYTHON3 = sys.version_info.major == 3
+
+
 class Quadrant(Enum):
     INIT = 0
     NW = 1
@@ -20,44 +26,67 @@ class Quadrant(Enum):
     NE = 4
 
 
-class AngleDetector():
+class AngleDetectorService:
     def __init__(self):
         rospy.init_node("Angle_detector")
-        #self.image_topic = "/realsense/rgb"
+        # self.image_topic = "/realsense/rgb"
+
+        self.AD = AngleDetector(writeImages=True, showImages=False, cv2Image=False)
+        # self.current_image = None
+        # rospy.Subscriber(self.image_topic, Image, self.image_callback)
+        rospy.Service("track_angle", AngleTrack, self.update_angle)
+        rospy.Service("reset_angle_tracking", Empty, self.reset_tracking)
+
+        rospy.loginfo("Angle tracker ready!")
+        rospy.spin()
+
+    def updateAngle(self, req):
+        return self.AD.update_angle(req.img)
+
+    def resetTracking(self, req):
+        self.AD.reset_tracking()
+        return EmptyResponse()
+
+
+class AngleDetector:
+    def __init__(self, writeImages=True, showImages=True, cv2Image=False):
 
         self.bridge = CvBridge()
         self.state = Quadrant.INIT
         self.closest_state = Quadrant.INIT
         self.angle = None
+        self.angular_velocity = 0
+        self.calc_time = None
+        self.writeImages = writeImages
+        self.showImages = showImages
+        self.cv2Image = cv2Image
 
-        #self.current_image = None
-        #rospy.Subscriber(self.image_topic, Image, self.image_callback)
-        rospy.Service('track_angle', AngleTrack, self.update_angle)
-        rospy.Service('reset_angle_tracking', Empty, self.reset_tracking)
-
-        rospy.loginfo("Angle tracker ready!")
-        rospy.spin()
-
-    # def image_callback(self, data):
-    #     self.current_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-
-    def reset_tracking(self, req):
+    def reset_tracking(self):
         self.state = Quadrant.INIT
         self.closest_state = Quadrant.INIT
         self.angle = None
-        print('reset!')
-        return EmptyResponse()
+        self.angular_velocity = None
+        print("reset!")
+
+    def getAngle(self):
+        return self.angle
+
+    def getAngularVelocity(self):
+        return self.angular_velocity
 
     def angle_calculation(self, point0, point1):
-        if point0[0]-point1[0] == 0:
+        if point0[0] - point1[0] == 0:
             temp_angle = 90
         else:
-            temp_angle = np.arctan(np.abs(float(point0[1]-point1[1])/float(point0[0]-point1[0])))*180/np.pi
+            temp_angle = (
+                np.arctan(
+                    np.abs(float(point0[1] - point1[1]) / float(point0[0] - point1[0]))
+                )
+                * 180
+                / np.pi
+            )
 
-        # print((point0[1]-point1[1]))
-        # print((point0[0]-point1[0]))
-        # print(np.abs((point0[1]-point1[1])/(point0[0]-point1[0])))
-        # print(self.state)
+        prev_angle = self.angle
 
         if self.state == Quadrant.NW:
             self.angle = -temp_angle
@@ -68,15 +97,29 @@ class AngleDetector():
         elif self.state == Quadrant.NE:
             self.angle = -180 + temp_angle
 
+        current_time = timeit.default_timer()
+
+        if self.calc_time:
+            new_angular_velocity = (self.angle - prev_angle) / (
+                current_time - self.calc_time
+            )
+            self.angular_velocity = (
+                0.8 * new_angular_velocity + 0.2 * self.angular_velocity
+            )
+
+        self.calc_time = current_time
+
     def state_update(self, point0, point1):
         rightmost_point = 0 if point0[0] > point1[0] else 1
         lowest_point = 0 if point0[1] > point1[1] else 1
 
         if self.state == Quadrant.INIT:
             self.state = Quadrant.NW if rightmost_point == lowest_point else Quadrant.SW
-            self.closest_state = Quadrant.SW if rightmost_point == lowest_point else Quadrant.NW
+            self.closest_state = (
+                Quadrant.SW if rightmost_point == lowest_point else Quadrant.NW
+            )
         elif self.state == Quadrant.NW:
-            print(rightmost_point, lowest_point, self.state, self.closest_state)
+            # print(rightmost_point, lowest_point, self.state, self.closest_state)
             if rightmost_point is not lowest_point:
                 self.state = self.closest_state
         elif self.state == Quadrant.SW:
@@ -89,11 +132,10 @@ class AngleDetector():
             if rightmost_point is lowest_point:
                 self.state = self.closest_state
 
-
     def closest_new_state(self):
         self.closest_state = Quadrant.INIT
         if self.state == Quadrant.NW:
-            self.closest_state = Quadrant.SW if self.angle > -45 else Quadrant.NE  
+            self.closest_state = Quadrant.SW if self.angle > -45 else Quadrant.NE
         elif self.state == Quadrant.SW:
             self.closest_state = Quadrant.NW if self.angle < 45 else Quadrant.SE
         elif self.state == Quadrant.SE:
@@ -101,85 +143,63 @@ class AngleDetector():
         elif self.state == Quadrant.NE:
             self.closest_state = Quadrant.NW if self.angle > -135 else Quadrant.SE
 
-
-    def update_angle(self, req):
-        # rate = rospy.Rate(0.5)
-
-        # while self.current_image is None and not rospy.is_shutdown():
-        #     rospy.sleep(1)
-
-        # while not rospy.is_shutdown():
-
-        print("Reached loop!")
+    def update_angle(self, im):
+        # print("Reached loop!")
         # image = cv2.imread('img_temp.jpeg')
-        im = self.bridge.imgmsg_to_cv2(req.im, desired_encoding='8UC3')
+        if not self.cv2Image:
+            im = self.bridge.imgmsg_to_cv2(im, desired_encoding="8UC3")
+
         result = im.copy()
         image = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-        
-        lower = np.array([0,85,0])
-        upper = np.array([7,255,255])
+
+        lower = np.array([0, 85, 0])
+        upper = np.array([7, 255, 255])
         mask = cv2.inRange(image, lower, upper)
 
-
-        lower1 = np.array([175,85,0])
-        upper1 = np.array([180,255,255])
+        lower1 = np.array([175, 85, 0])
+        upper1 = np.array([180, 255, 255])
         mask1 = cv2.inRange(image, lower1, upper1)
 
         mask = mask + mask1
 
         result = cv2.bitwise_and(result, result, mask=mask)
 
-        cv2.imshow('orig', im)
-        cv2.imshow('hsv', image)
-        cv2.imshow('mask', mask)
+        if self.showImages:
+            cv2.imshow("orig", im)
+            cv2.imshow("hsv", image)
+            cv2.imshow("mask", mask)
+            cv2.waitKey(1)
 
-        cv2.imwrite('img_temp.jpeg', im)
+        if self.writeImages:
+            cv2.imwrite("img_temp.jpeg", im)
+            cv2.imwrite("mask.jpeg", mask)
+            cv2.imwrite("result.jpeg", result)
 
-        cv2.imwrite('mask.jpeg', mask)
-        # cv2.imshow('result', result)
-        cv2.imwrite('result.jpeg', result)
+        contours = None
+        if PYTHON3:
+            contours, _ = cv2.findContours(
+                mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+            )
+            # print(tmp, contours)
+            contours = sorted(
+                contours, key=lambda el: cv2.contourArea(el), reverse=True
+            )
 
-        # params = cv2.SimpleBlobDetector_Params()
-        # params.filterByArea = True
-        # params.minArea = 100
-        # params.maxArea = 2000
+        else:
+            _, contours, _ = cv2.findContours(
+                mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+            )
+            contours.sort(key=lambda el: cv2.contourArea(el), reverse=True)
 
-        # detector = cv2.SimpleBlobDetector_create()
-        # keypoints = detector.detect(mask)
-        # im_with_keypoints = cv2.drawKeypoints(mask, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        # cv2.imshow("Keypoints", im_with_keypoints)
-        # cv2.waitKey(0)
-
-        _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        # blob = max(contours, key=lambda el: cv2.contourArea(el))
-        # print(type(contours))
-        contours.sort(key=lambda el: cv2.contourArea(el), reverse=True)
-        blob_list = contours
-        # print(blob_list)
-        blob = blob_list[0]
-        M = cv2.moments(blob)
-        center1 = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        # print(center)
         canvas = result.copy()
-        cv2.circle(canvas, center1, 2, (0,255,0), -1)
 
-        blob2 = blob_list[1]
-        M = cv2.moments(blob2)
+        M = cv2.moments(contours[0])
+        center1 = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        cv2.circle(canvas, center1, 2, (0, 255, 0), -1)
+
+        M = cv2.moments(contours[1])
         center2 = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        # print(center)
-        cv2.circle(canvas, center2, 2, (0,255,0), -1)
-
-        # print(center1)
-        # print(center2)
-        # if center1[0] > center2[0]:
-        #     angle = np.arctan2(center2[1]-center1[1], center2[0]-center1[0])
-        # else:
-        #     angle = np.arctan2(center1[1]-center2[1], center1[0]-center2[0])
-
-        # print(angle*180/np.pi)
-
-        # angle = self.angle_calculation(center1, center2)
-        # print(angle)
+        cv2.circle(canvas, center2, 2, (0, 255, 0), -1)
 
         if self.angle is None:
             self.state_update(center1, center2)
@@ -189,23 +209,18 @@ class AngleDetector():
             self.state_update(center1, center2)
             self.angle_calculation(center1, center2)
 
-        print(self.angle)
+        if self.showImages:
+            cv2.imshow("canvas", canvas)
+            cv2.waitKey()
 
-
-        cv2.imshow('canvas', canvas)
-        cv2.imwrite('canvas.jpeg', canvas)
-        cv2.waitKey(1)
-
-
-            # cv2.waitKey()
-
-            # rate.sleep()
+        if self.writeImages:
+            cv2.imwrite("canvas.jpeg", canvas)
 
         return self.angle
 
 
 if __name__ == "__main__":
     try:
-        angle_class = AngleDetector()
+        angle_class = AngleDetectorService()
     except KeyboardInterrupt:
         pass
